dcfcf6983ddf34fa1fee476112b916ae2e85000a
Rohan Kulkarni <rohank2502@gmail.com>
2024-02-09 20:15:03 +0530

    Added React Router  Dom Setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Hello Login!</div>,
+  },
+  {
+    path: "/dashboard",
+    element: <div>Hello Dashboard!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <RouterProvider router={router} />
+  </>
+);
