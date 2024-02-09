0dd86582311f60a644166ede10373af4c049b754
Rohan Kulkarni <rohank2502@gmail.com>
2024-02-09 20:11:50 +0530

    Added Tailwind CSS to vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      {/* <h1>Hello Vaishnavi</h1> */}
-    </>
+    <h1 className="bg-red-400 text-3xl font-bold underline">Hello world!</h1>
   );
 }
-
-export default App;
